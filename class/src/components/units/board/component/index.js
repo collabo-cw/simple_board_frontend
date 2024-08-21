79ceffed73a74c12e68a9a0aca5b29549c110ecd
Son Woo Heon <b29299461@gmail.com>
2024-08-21 17:46:30 +0900

    ui 변경, props (게시글수정, 작성) 연동

--- class/src/components/units/board/component/index.js
+++ class/src/components/units/board/component/index.js
@@ -1,12 +1,22 @@
-import { Button } from "antd"
+import axios from "axios";
+import { useState } from "react";
+import {Container,Wrapper,WrapLogin,Title,H4,Textarea} from '../../../../../styles/new.styles';
+export default function BoardNewPage(props) {
 
-export default function BoardComponent(props){
     return(
-        <div>
-            <h1>{props.isEdit? "수정" :"등록"}페이지</h1>
-            제목:<input type="text"/><br/>
-            내용: <input type="text"/><br/>
-            <button>{props.isEdit? "수정" :"등록"}하기</button>
-        </div>
+        <Container>
+            <Wrapper>
+                <WrapLogin>
+                    <Title>게시글 {props.isEdit? "수정" : "작성"}</Title>
+                    <div>
+                        <H4>작성자</H4>
+                        <input type="text"/>
+                        <H4>내용</H4>
+                        <Textarea type="text" />
+                    </div>
+                    <button>{props.isEdit? "수정" : "작성"}하기</button>
+                </WrapLogin>
+            </Wrapper>
+        </Container>
     )
 }
