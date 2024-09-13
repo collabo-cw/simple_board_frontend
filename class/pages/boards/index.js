1dff4801767b6ac0de5b10487c79c4ec388ce87f
Son Woo Heon <b29299461@gmail.com>
2024-09-13 13:55:37 +0900

    메인화면 UI 만들기

--- class/pages/boards/index.js
+++ class/pages/boards/index.js
@@ -1,29 +1,39 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default function ListPasges(){
+export default function ListPasges() {
+    // 상태를 정의합니다
+    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('');
 
     const onClickGuestSubmit = async (e) => {
         try {
-            const result = await axios.post('https://944e-121-140-170-17.ngrok-free.app/board/get/board-detail',{
+            const response = await axios.post('https://944e-121-140-170-17.ngrok-free.app/board/get/board-detail', {
                 id: 1,
-                category:"GENERAL"
-            })
-            console.log("성공"); // data 속성을 확인합니다
+                category: "GENERAL"
+            });
+            
+            // 응답 데이터에서 guest_author와 title을 추출하여 상태를 업데이트합니다
+            const { guest_author, title } = response.data.result;
+            setAuthor(guest_author);
+            setTitle(title);
+
+            console.log("성공"); // 데이터가 성공적으로 받아졌음을 확인합니다
+            
         } catch (error) {
             console.error(error);
-            console.log("실패")
+            console.log("실패");
         }
     };
 
     return (
-        <div style={{ fontSize: "30px"}}>
+        <div style={{ fontSize: "30px" }}>
             목록
             <button onClick={onClickGuestSubmit}>버튼</button>
+            <div>
+                <div>작성자: {author}</div>
+                <div>제목: {title}</div>
+            </div>
         </div>
-    )
+    );
 }
-
-
-
-
--- class/pages/main/index.js
+++ class/pages/main/index.js
@@ -3,209 +3,29 @@ import styled from "@emotion/styled";
 import { useState } from "react";
 import { useRouter } from 'next/router'
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-  max-width: 768px;
-`;
-const Wrapper = styled.div`
   width: 100%;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   box-sizing: border-box;
-  background-image: url("../img/pizza.jpg");
-  background-color: rgba(0, 0, 0, 0.55);
-  background-blend-mode: multiply;
-  background-size:contain;
+  background: url("../img/frame.png") no-repeat;
+  background-size:100% auto;
+  max-width: 500px;
+  min-height:770px;
+  margin:0 auto;
+  padding:120px 40px 0;
 `;
-const LogoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 90px;
-  padding-bottom: 70px;
+const Wrap = styled.div`
+  display: block;
 `;
-const LogoImg = styled.img`
-  width: 100px;
-  height: 100px;
-`;
-const LogoTitle = styled.div`
-  font-size: 30px;
-  font-weight: bold;
-  color: #fff;
-  padding-top: 10px;
-`;
-const InputWrapper = styled.div`
-  width: 100%;
-`;
-const EmailInputWrapper = styled.div`
-  border-bottom: 1px solid #fff;
-  padding-bottom: 10px;
-  margin-bottom: 10px;
-`;
-const EmailInput = styled.input`
-  width: 80%;
-  font-size: 14px;
-  color: #fff;
-  border: none;
-  background: none;
-  outline: none;
-`;
-const PasswordInputWrapper = styled.div`
-  border-bottom: 1px solid #fff;
-  padding-bottom: 10px;
-  margin-bottom: 10px;
-`;
-const PasswordInput = styled.input`
-  width: 80%;
-  margin-top: 40px;
-  font-size: 14px;
-  color: #fff;
-  border: none;
-  background: none;
-  outline: none;
-`;
-const EmailError = styled.div`
-  padding-top: 10px;
-  font-size: 12px;
-  color: #ff1b6d;
-`;
-const PasswordError = styled.div`
-  padding-top: 10px;
-  font-size: 12px;
-  color: #ff1b6d;
-`;
-const LoginButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 45px;
-  border-radius: 38px;
-  background-color: rgba(255, 27, 109, 0.6);
-  cursor: pointer;
-  margin-top:15px;
-  margin-bottom:15px;
-`;
-const LoginButtonTitle = styled.div`
-  opacity: 1;
-  font-size: 16px;
-  font-weight: bold;
-  color: #fff;
-`;
-const EtcButton = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-`;
-const EtcItem = styled.div`
-  font-size: 14px;
-  color: #fff;
-  cursor: pointer;
-`;
-const SocialLoginButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 50px;
-  border-radius: 38px;
-  color: #fae100;
-  border: 2px solid #fae100;
-  margin: 60px 0 80px 0;
-  cursor: pointer;
-`;
-const SocialLoginButtonImg = styled.img`
-  width: 32px;
-  height: 30px;
-  margin-right: 20px;
-`;
-const SocialLoginButtonTitle = styled.div`
-  opacity: 1;
-  font-size: 16px;
-  font-weight: bold;
-`;
-
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
 
-  const router =  useRouter();
-  const onChangeEmail = (event) => {
-    setEmail(event.target.value);
-    if (!email.includes("@")) {
-      setEmailError("이메일 주소를 다시 확인해주세요.");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const onChangePassword = (event) => {
-    setPassword(event.target.value);
-    if (password.length >= 8 && password.length <= 16) {
-      setPasswordError("");
-    } else {
-      setPasswordError("8~16자의 영문,숫자,특수 문자만 사용 가능합니다.");
-    }
-  };
-  const onClickLogin = async () => {
-    if (emailError === "" && passwordError === "") {
-      alert("환영합니다");
-    } 
-    const result = await axios.get("https://koreanjson.com/posts/1")     //함수 중복선언 문제
-        console.log(result) // 제대로된 결과 => { title: "..."}
-        console.log(result.data.title) // 제대로된 결과 => { title: "..."}
-  };
-
-  const onClickMembership = () => {
-    router.push('./membership')
-    alert("회원가입 페이지로 이동합니다")
-  }
 
   return (
-    <Container>
-      <Wrapper>
-        <LogoWrapper>
-          <LogoImg src="/img/Logo.png"></LogoImg>
-          <LogoTitle>잇츠로드</LogoTitle>
-        </LogoWrapper>
-        <InputWrapper>
-          <EmailInputWrapper>
-            <EmailInput
-              placeholder="이메일을 입력해주세요"
-              onChange={onChangeEmail}
-            ></EmailInput>
-          </EmailInputWrapper>
-          <EmailError>{emailError}</EmailError>
-          <PasswordInputWrapper>
-            <PasswordInput
-              placeholder="비밀번호를 입력해주세요"
-              type="password"
-              onChange={onChangePassword}
-            ></PasswordInput>
-          </PasswordInputWrapper>
-          <PasswordError>{passwordError}</PasswordError>
-        </InputWrapper>
-        <LoginButton onClick={onClickLogin}>
-          <LoginButtonTitle>로그인</LoginButtonTitle>
-        </LoginButton>
-        <EtcButton>
-          <EtcItem>이메일 찾기</EtcItem>
-          <EtcItem>|</EtcItem>
-          <EtcItem>비밀번호 찾기</EtcItem>
-          <EtcItem>|</EtcItem>
-          <EtcItem onClick={onClickMembership}>회원가입</EtcItem>
-        </EtcButton>
-        <SocialLoginButton>
-          <SocialLoginButtonImg src="../img/kakao.png"></SocialLoginButtonImg>
-          <SocialLoginButtonTitle>카카오톡으로 로그인</SocialLoginButtonTitle>
-        </SocialLoginButton>
-      </Wrapper>
-    </Container>
+
+      <Container>
+        <Wrap>
+          언제 다 만드니 이걸 ㅠㅠㅠㅠㅠ
+        </Wrap>
+      </Container>
+
   );
 }
