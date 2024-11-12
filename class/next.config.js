926ef6be5f226b34e3458c6309c56095af99a2ca
Son Woo Heon <b29299461@gmail.com>
2024-11-12 10:51:47 +0900

    json 수정

--- class/next.config.js
+++ class/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  //2024-11-12 추가 start
+  basePath: '/simple_board_frontend', // GitHub Pages에서 사용할 경로
+  images: {
+    unoptimized: true, // 이미지 최적화 비활성화 (정적 사이트에서 필요)
+  },
+  //2024-11-12 추가 end
 };
 
 module.exports = nextConfig;
