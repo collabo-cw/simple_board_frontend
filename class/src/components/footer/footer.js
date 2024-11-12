867a34650345de3eab490a6fb6568121577fa5fb
Son Woo Heon <b29299461@gmail.com>
2024-11-12 12:43:50 +0900

    ㅁㄴㅇㅁㄴㅇ

--- class/src/components/footer/footer.js
+++ class/src/components/footer/footer.js
@@ -35,11 +35,11 @@ const Footer = styled.footer`
 `;
 
 const menuItems = [
-    { id: 0, label: "홈", Icon: RiHome2Line, href: "./main" },
-    { id: 1, label: "검색", Icon: IoSearchOutline, href: "../simple_board_frontend/main/search"},
-    { id: 2, label: "예약", Icon: HiCalendar, href: "../simple_board_frontend/main/reservation" },
-    { id: 3, label: "커뮤니티", Icon: BiMessageRoundedDetail , href: "../simple_board_frontend/main/community" },
-    { id: 4, label: "마이", Icon: GoPeople , href:"./login"}
+    { id: 0, label: "홈", Icon: RiHome2Line, href: "../main" },
+    { id: 1, label: "검색", Icon: IoSearchOutline, href: "../main/search"},
+    { id: 2, label: "예약", Icon: HiCalendar, href: "../main/reservation" },
+    { id: 3, label: "커뮤니티", Icon: BiMessageRoundedDetail , href: "../main/community" },
+    { id: 4, label: "마이", Icon: GoPeople , href:"../login"}
 ];
 
 export default function FooterComponent() {
